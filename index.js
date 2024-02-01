f3b3a881616b1fd94bb6f12b1e142133974e901a
William <william@inapulse.com>
2024-02-01 16:54:03 -0500

    Notification passed beta testing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ['https://www.faceclam.com', 'https://faceclam.com', 'http://localhost:3000'],
   },
 });
 const user = require("./controller/user");
