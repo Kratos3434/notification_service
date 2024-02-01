45e459743036089d0e15dc3111ab7043c81889ec
William <william@inapulse.com>
2024-02-01 14:05:24 -0500

    Socket notification server

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
-const port = process.env.PORT || 8080;
-const express = require('express');
+const port = process.env.PORT || 8081;
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000"
-  }
+    origin: "http://localhost:3000",
+  },
 });
+const user = require("./controller/user");
 
 let users = [];
 
-io.on('connection', (socket) => {
-  console.log("A user connected");
-  socket.on('join', (data) => {
-    socket.data.email = data.email;
-    if (!users.find((e) => {
-      return e.email === socket.data.email;
-    })) {
-      users.push({email: socket.data.email, notifications: []});
-    }
-    console.log("Users:", users);
-    socket.join(data.email);
-  })
+const findUser = (email) => {
+  return users.find((e) => {
+    return e.email == email;
+  });
+};
 
-  socket.on("like", (data) => {
-    console.log("Like event")
-    const idx = users.findIndex(e => e.email === data.to);
-    users[idx].notifications.push(`${data.liker} has liked your post`);
-    socket.to(data.to).emit('notif', {
-      notifications: users[idx].notifications
-    })
-  })
-  socket.on('disconnect', () => {
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("join", async (data) => {
+    socket.data.email = data.email;
+    let notifications = null;
+    if (!findUser(socket.data.email)) {
+      users.push({ email: socket.data.email });
+    }
+    socket.join(data.email);
+    // socket.to(data.email).emit("notifications", {
+    //   notifications,
+    // });
+    // socket.emit("notifications", {
+    //   notifications
+    // })
+    console.log("Users:", users);
+  });
+
+  socket.on("like", async (data) => {
+    console.log("Like event");
+    if (data.recipientId != data.senderId) {
+      const notification = await user.sendNotification(
+        data.senderId,
+        data.recipientId,
+        data.type,
+        `${data.sender} has liked your post`,
+        data.postId
+      );
+      if (notification) {
+        socket.to(data.recipient).emit("notifications", {
+          notifications: null,
+        });
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
     console.log(`Disconnected: ${socket.data.email}`);
-    users = users.filter(e => e.email !== socket.data.email);
+    users = users.filter((e) => e.email !== socket.data.email);
     console.log("Disconnect:", users);
     console.log("A user disconnected");
-  })
-
-})
+  });
+});
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
